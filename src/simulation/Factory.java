93e661e75d5de1be8c3c2c3f9dd73f446488704a
Leonard K. Ng'eno <dranoel.bravo@gmail.com>
2013-02-06 18:45:48 -0500

    removed cached bin/

--- src/simulation/Factory.java
+++ src/simulation/Factory.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-
 import simulation.elements.masses.FixedMass;
 import simulation.elements.masses.Mass;
 import simulation.elements.springs.Muscle;
