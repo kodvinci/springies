e833a4662394adde9cf2ac0af5dfeb5034671c82
Erick Gonzalez <erick@erick-K52Jc.(none)>
2013-02-06 17:26:17 -0500

    Made certain fields 'private static final' in the Muscle.java class, also changed the name of BETA to myAmplitude

--- src/simulation/elements/springs/Muscle.java
+++ src/simulation/elements/springs/Muscle.java
@@ -15,26 +15,29 @@ import util.Vector;
  */
 public class Muscle extends Spring {
 	// default values
-	private double BETA; // Takes on values between 0 and 1. Fixes wave
-							// amplitude
-	private double OMEGA = 30; // Speed of oscillation of muscles
-	private double PHI = Math.PI / 2; // Can take on 0, Math.PI/2, Math.PI/3,
-										// 3*Math.PI/4 radians
-	private double ALPHA = 1; // Assumes values between 1 and 0. relative
-								// amplitude
+	private static final double OMEGA = 30; // Speed of oscillation of muscles
+	private static final double PHI = Math.PI / 2; // Can take on 0, Math.PI/2,
+													// Math.PI/3,
+													// 3*Math.PI/4 radians
+	private static final double ALPHA = 1; // Assumes values between 1 and 0.
+											// relative
+											// amplitude
 
 	private double restLength;
+	private double myAmplitude; // Takes on values between 0 and 1. Fixes wave
+								// amplitude
 
 	public Muscle(Mass start, Mass end, double length, double kVal,
 			double amplitude) {
 		super(start, end, length, kVal);
 		restLength = length;
-		BETA = amplitude;
+		myAmplitude = amplitude;
 	}
 
 	@Override
 	public void update(double elapsedTime, Dimension bounds) {
-		setMyLength(restLength * (1 + ALPHA * BETA * Math.sin(OMEGA + PHI)));
+		setMyLength(restLength
+				* (1 + ALPHA * myAmplitude * Math.sin(OMEGA + PHI)));
 
 		double dx = myStart().getX() - myEnd().getX();
 		double dy = myStart().getY() - myEnd().getY();
